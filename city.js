0146df3929d270ab31df4880b2743da791f4d909
TrevorFrame <43511442+TrevorFrame@users.noreply.github.com>
2019-05-20 20:54:22 -0700

    update js files
    combine js functions onto correct js files

--- city.js
+++ city.js
@@ -33,5 +33,22 @@ module.exports = function(){
 		}
 	});
 	
+	/* Adds a city, then redirects back to the explore-cities page*/
+	router.post('/', function(req, res){
+		console.log(req.body)
+		var mysql = req.app.get('mysql');
+		var sql = "INSERT INTO City (name, state) VALUES (?,?)";
+		var inserts = [req.body.name, req.body.state];
+		sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+			if(error){
+				console.log(JSON.stringify(error))
+				res.write(JSON.stringify(error));
+				res.end();
+			}else{
+				res.redirect('/explore-cities'); //Should this be /explore-cities.html?
+			}
+		});
+	});
+	
 	return router;
 }();
